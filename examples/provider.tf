2c0f302afbb646aaaf8cff643426c28780c41831
Carl Hattingh <carl.hattingh@harrison.ai>
2024-06-10 20:47:00 +1000

    update providers

--- examples/provider.tf
+++ examples/provider.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = ">= 5.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.3.2"
+      version = "3.6.2"
     }
   }
 }
