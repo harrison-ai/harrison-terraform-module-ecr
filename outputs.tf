539f169dd04f56744500b765e0fdd5c2acbf0e39
daniellohrey-hai <117246481+daniellohrey-hai@users.noreply.github.com>
2024-10-10 11:38:42 +1100

    Add repository arn to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "repository_url" {
   value       = aws_ecr_repository.this.repository_url
   description = "ECR Repository URL"
 }
+
+output "repository_arn" {
+  value       = aws_ecr_repository.this.arn
+  description = "ECR Repository ARN"
+}
